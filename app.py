8b3e677bdfdde906063d42a7094083a9ff70f829
a355226 <a355226@hotmail.com>
2025-04-01 22:20:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,8 @@ def handle_message(event):
             )
         )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
